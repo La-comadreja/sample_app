b6fc8f1281997ae286e159992f12fd1f5f51fc49
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-07-28 19:10:30 -0400

    Update capybara

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -14,7 +14,7 @@ SampleApp::Application.configure do
   config.static_cache_control = "public, max-age=3600"
 
   # Log error messages when you accidentally call methods on nil
-  config.whiny_nils = true
+#  config.whiny_nils = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
