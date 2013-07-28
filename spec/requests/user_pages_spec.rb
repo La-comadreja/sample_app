3ddb9d68b1744e9521b675d4ff3a2e23c56584ce
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-07-28 18:39:42 -0400

    To 7.1.4; tests still fail

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,7 +12,7 @@ describe "User pages" do
   end
 
   describe "profile page" do
-    # Replace with code to make a user variable
+    let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
