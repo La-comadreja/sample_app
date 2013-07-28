7c5713a21b915053f695be6e33071b9f4e82c5f0
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-07-28 18:26:57 -0400

    Revert the spec back to old

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ SampleApp::Application.routes.draw do
 
   root to: 'static_pages#home'
 
-  get '/signup',  to: 'users#new'
+  match '/signup',  to: 'users#new', via: 'get'
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
